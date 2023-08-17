c237d58f957840196b0686f7fcaa0991b531b037
NoiseBOX <noisebox@gmail.com>
2023-08-17 00:46:39 -0700

    First compile errors

--- Plugins/ArpBOX/PluginProcessor.h
+++ Plugins/ArpBOX/PluginProcessor.h
@@ -6,15 +6,7 @@ class AudioPluginAudioProcessor : public juce::AudioProcessor {
   public:
     const juce::String editorIsVisibleId = "editorIsVisible";
     const juce::String editorIsVisibleName = "Editor Is Visible";
-/*     const juce::String parameter1Id = "parameter1";
-    const juce::String parameter1Name = "Parameter 1";
-    const juce::String parameter2Id = "parameter2";
-    const juce::String parameter2Name = "Parameter 2";
-    const juce::String parameter3Id = "parameter3";
-    const juce::String parameter3Name = "Parameter 3";
-    const juce::String parameter4Id = "parameter4";
-    const juce::String parameter4Name = "Parameter 4"; */
-    //==============================================================================
+
     enum ChordShapes { MAJOR, MINOR /*, ... */ };
     enum ArpPatterns { UP, DOWN, PING_PONG, RANDOM };
     enum NoteDivision { QUARTER, EIGHTH, SIXTEENTH, THIRTY_SECOND };
@@ -22,12 +14,6 @@ class AudioPluginAudioProcessor : public juce::AudioProcessor {
     const juce::StringArray chordShapeNames = {"Major", "Minor" /*, ... */};
     const juce::StringArray arpPatternNames = {"Up", "Down", "Ping Pong", "Random"};
     const juce::StringArray noteDivisionNames = {"1/4", "1/8", "1/16", "1/32"};
-
-    static const juce::StringArray chordShapeNames;
-    static const juce::StringArray arpPatternNames;
-    static const juce::StringArray noteDivisionNames;
-
-    juce::AudioProcessorValueTreeState state;
 
     std::vector<int> generateChordNotes(int rootNote, ChordShapes chordShape, int octaves);
     std::vector<int> applyArpPattern(const std::vector<int>& chordNotes, ArpPatterns arpPattern);
